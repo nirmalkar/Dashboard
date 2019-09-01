5257793a955d42a8f73a53e9796a25dd2cf737bd
Hemant Nirmalkar <hemantnirmalkar17@gmail.com>
2019-09-01 13:31:20 +0530

    Toggle Function Added

--- script.js
+++ script.js
@@ -3,3 +3,17 @@ const messageBtn = document.querySelector(".messageBtn")
 messageBtn.addEventListener("click", function () {
   alert("hello");
 });
+
+const toggleBtn = document.querySelector("#toggleBtn")
+const sidebar = document.querySelector("#sidebar")
+const contentContainer = document.querySelector("#content-container")
+
+toggleBtn.addEventListener('click', () => {
+  if (sidebar.style.display === 'none') {
+    sidebar.style.display = 'block'
+    contentContainer.className = 'col-lg-9 h-100vh'
+  } else {
+    sidebar.style.display = 'none'
+    contentContainer.className = 'col-12'
+  }
+})
