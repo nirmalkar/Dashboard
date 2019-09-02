fcca5d5db1e13e30d088542f5e0d1ccc9c57fb42
Hemant Nirmalkar <hemantnirmalkar17@gmail.com>
2019-09-02 21:04:28 +0530

    top navbar updated- toggle feature added to the top navbar

--- script.js
+++ script.js
@@ -5,10 +5,23 @@ messageBtn.addEventListener("click", function () {
 });
 
 const toggleBtn = document.querySelector("#toggleBtn")
+const toggleBtn1 = document.querySelector("#toggleBtn1")
 const sidebar = document.querySelector("#sidebar")
 const contentContainer = document.querySelector("#content-container")
 
 toggleBtn.addEventListener('click', () => {
+  if (sidebar.style.display === 'none') {
+    sidebar.style.display = 'block'
+    contentContainer.className = 'col-lg-9 h-100vh'
+  } else {
+    sidebar.style.display = 'none'
+    contentContainer.className = 'col-12'
+  }
+})
+
+
+
+toggleBtn1.addEventListener('click', () => {
   if (sidebar.style.display === 'none') {
     sidebar.style.display = 'block'
     contentContainer.className = 'col-lg-9 h-100vh'
